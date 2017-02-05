f2ecd398eaa94d22b216e02a663cd35c8fa5bcc9
alish <khassenalisher@gmail.com>
2017-02-05 23:16:07 +0600

    Comlex

---
+++ Week2/Task4.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Complex
+    {
+        int a;
+        int b;
+        public Complex(int a,int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public static Complex operator +(Complex C1, Complex C2)
+        {
+            Complex res = new Complex(0, 0);
+
+            res.a = C1.a + C2.a;
+            res.b = C1.b + C2.b;
+            return res;
+        }
+        public override string ToString()
+        {
+            return string.Format("{0}+{1}i",a,b);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex c1 = new Complex(2, 4);
+            Complex c2 = new Complex(2, 7);
+            Console.WriteLine(c1 + c2);
+            Console.ReadKey();
+        }
+    }
+}
