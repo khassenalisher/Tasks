c5e1ff15751918446a7899560d9c4eb77c527ffd
alish <khassenalisher@gmail.com>
2017-02-05 21:01:06 +0600

    GCD by REC

---
+++ Week2/Task3Rec.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3_Rec_
+{
+    class Program
+    {
+        // Созадем функцию для нахождения наибольшего общего делителя двух заданных чисел
+        static int Rec(int n,int m)
+        {
+            int l;
+            if (n == 0)
+            {
+                Console.WriteLine(m);
+                return 0;
+            }
+            else Console.Write("");//Спросить про Else
+            if(n>m)
+            {
+                l = m;
+                m = n;
+                n = l;
+            }
+            else Console.Write("");
+            Rec(m - n, n);
+            return 0;
+        }
+        static void Main(string[] args) 
+        {
+            //Вызываем функцию для нахождения наибольшегго общего делителя
+            Rec(25, 100);
+            Console.ReadKey();    
+        }
+    }
+}
