fe33c0453353d8325a04ff2fa6b2e7e5d02e06e5
alish <khassenalisher@gmail.com>
2017-02-05 22:28:15 +0600

    GCD by Stack

---
+++ Week2/Task3Stack.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            var Stack = new Stack<int>();
+            //Алгоритм Евклида вычитание большего от меньшего
+            while (a != b)
+            {
+                Stack.Push(a);
+                if (a > b)
+                {
+                    a = a - b;
+                }
+                else
+                {
+                    b = b - a;
+                }
+            }
+            //Добавялем в стэк значение "а" в последний раз который и является ответом
+            Stack.Push(a);
+            Console.WriteLine(Stack.Peek());
+
+
+            Console.ReadKey();
+        }
+    }
+}
