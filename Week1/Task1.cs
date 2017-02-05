76f5a5dc9ce519049bf025529e927f4d6c4a7d81
alish <khassenalisher@gmail.com>
2017-02-05 17:33:53 +0600

    Week1

---
+++ Week1/Task1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        // создаем функцию которая будет проверять является ли данное число простым
+        // если простое то будет возвращать тrue
+        // иначе false
+        static bool Prime(string x)
+        {
+            // так как мы задали х стринговым то нам нужно получить числовое значение
+            int a = int.Parse(x);
+            int cnt = 0;
+            // создаем счетчик который будет увеличиваться каждый раз когда каждый раз находим
+            //делитель данного числа, для этого мы пробигаемся от 1 до данного числа включительно 
+            // соответсвеено если данный счетчик будет равен 2 то данное число ПРОСТОЕ
+            // этот код можно доработать но на данный момент я его оставлю таким
+            for (int i = 1; i <= a; i++)
+            {
+                if (a % i == 0)
+                {
+                    cnt++;
+                }
+            }
+            if (cnt == 2) return true;
+            else
+                return false;
+
+        }
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; ++i)
+            {
+                if (Prime(args[i]))
+                {
+                    Console.WriteLine(args[i]);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Week1/Task2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Student
+    {
+        public int gpa;
+        public string name;
+        public string lastname;
+
+        public Student()
+        {
+
+        }
+
+        public Student(string lastname, string name, int gpa)
+        {
+            //присваиваем заданные значения
+            this.lastname = lastname;
+            this.name = name;
+            this.gpa = gpa;
+        }
+
+        public override string ToString()
+        {
+            //возрвращаем заданные значения в нужном виде
+            return lastname + " " + name + ": " + gpa;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student("Khassen", "Alisher", 3);
+            Console.WriteLine(s.ToString());
+            Console.ReadKey();
+        }
+    }
+}
